917a1ad26dfe2621b047aff0d64cceccca104473
Enock Tangus <tanguskipngeno@gmail.com>
2025-03-06 12:55:53 +0300

    arithmetic lab complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+// multiply
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+console.log(multiply);
+
+// random
+const random = Math.floor(Math.random() * 10) + 1;
+
+// mod
+let num3 = 16
+let num4 = 6
+const mod = num3 % num4
+console.log(mod);
+
+// max
+const max = Math.max(3,6,8,9,13,17,18,20);
+console.log(max);
